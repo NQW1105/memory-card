2ecda15636305cd558d8df609e722f98b87d899b
NQW1105 <neoqw@hotmail.com>
2023-01-30 16:25:03 +0800

    Included tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
 
+const zodiac = [
+  '鼠 shǔ Rat',
+  '牛 niú Ox',
+  '虎 hǔ Tiger',
+  '兔 tù Rabbit',
+  '龙 lóng Dragon',
+  '蛇 shé Snake',
+  '马 mǎ Horse',
+  '羊 yáng Goat',
+  '猴 hóu Monkey',
+  '鸡 jī Rooster',
+  '狗 gǒu Dog',
+  '猪 zhū Pig',
+];
+
+// Red color, Responsive nav header bar
+// Golden text with custom font
+// 12 cards representing zodiac, red card with golden word
+// Every card clicked should shuffle these cards
+// Player score (out of 12)
+// if player lose or done with 12, reset the game
+
 function App() {
   return (
     <div className="App">
